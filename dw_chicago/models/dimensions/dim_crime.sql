9659bff12a95df09c8089f63c6706f3a175a45d0
Karine Victória Gomes <karine.gomes@estudante.ufjf.br>
2026-07-11 22:30:50 -0300

    arrumando gitignore

--- dw_chicago/models/dimensions/dim_crime.sql
+++ dw_chicago/models/dimensions/dim_crime.sql
@@ -1,15 +1,24 @@
-SELECT DISTINCT
+WITH crimes AS (
+
+    SELECT DISTINCT
+        iucr,
+        tipo_primario,
+        descricao,
+        codigo_fbi
+
+    FROM {{ ref('stg_crimes') }}
+
+)
+
+SELECT
 
     ROW_NUMBER() OVER (
         ORDER BY iucr, tipo_primario
     ) AS sk_crime,
 
     iucr,
-
     tipo_primario,
-
     descricao,
-
     codigo_fbi
 
-FROM {{ ref('stg_crimes') }}
+FROM crimes
